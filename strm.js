4ef8ddb6ddb8cc4b24c99ea0ce41d86d507e74d0
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-03-09 18:46:15 +0100

    Config support
    Add configuration and a reformated example credentials file from the aws docs

--- strm.js
+++ strm.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 
 const mainpath = process.argv[1].slice(0,-7)
 
+const config = JSON.parse(fs.readFileSync(`${mainpath}config.json`))
+
 if(process.argv.length>2&&!process.argv.join('/').includes('..')){
     const path = `${mainpath}api/${process.argv[2]}.js`
     if(fs.existsSync(path)){
