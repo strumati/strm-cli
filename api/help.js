e75fd9dec147ca9ea1723cb9cc4f85aed1c05335
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-06-02 14:28:03 +0200

    Implement help command for CLI
    Added a list of services and a description for the CLI.

--- api/help.js
+++ api/help.js
@@ -1,1 +1,18 @@
-console.log('not implemented')
+const services = [
+    "help",
+]
+
+services.sort()
+
+console.log(`  strm
+  ^^^^
+
+Description
+***********
+A Command-Line Interface for 
+Interaction with Cloud Services
+
+Available Commands
+******************
+
+${services.map(v=>`*${v}`).join('\n\n')}`)
